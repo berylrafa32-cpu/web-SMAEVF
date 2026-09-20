7c6f779678eae33b2bc4139a3623ba28f7de665e
berylrafa32-cpu <berylrafa32@gmail.com>
2026-09-20 11:49:43 +0700

    Add manual migration route

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ use App\Http\Controllers\TestimonialController;
 Route::get('/', [TestimonialController::class, 'index'])->name('home');
 Route::post('/testimoni', [TestimonialController::class, 'store'])->name('testimoni.store');
 Route::delete('/testimoni/{id}', [TestimonialController::class, 'destroy'])->name('testimoni.destroy');
+Route::get('/run-migration', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    return 'Migration berhasil dijalankan!';
+});
